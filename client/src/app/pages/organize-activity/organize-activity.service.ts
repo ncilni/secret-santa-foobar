2b8ac9402785d332fe5d6dc5822b40bd46c1139f
Neeraj Chauhan <ncilni@gmail.com>
2020-05-29 02:19:53 +0530

    fix in sefvice call

--- client/src/app/pages/organize-activity/organize-activity.service.ts
+++ client/src/app/pages/organize-activity/organize-activity.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class OrganizeActivityService {
-  private activityUrl = `/activity/organize`;
+  private activityUrl = `/api/activity/organize`;
 
   constructor(private http: HttpClient) {}
 
