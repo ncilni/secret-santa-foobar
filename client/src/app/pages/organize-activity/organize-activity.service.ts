854b24ff705f706fa552a51adb9e070a0c27d1d8
Neeraj Chauhan <ncilni@gmail.com>
2020-05-29 02:15:24 +0530

    small fixes

--- client/src/app/pages/organize-activity/organize-activity.service.ts
+++ client/src/app/pages/organize-activity/organize-activity.service.ts
@@ -1,12 +1,11 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class OrganizeActivityService {
-  private activityUrl = `${environment.apiUrl}/activity/organize`;
+  private activityUrl = `/activity/organize`;
 
   constructor(private http: HttpClient) {}
 
